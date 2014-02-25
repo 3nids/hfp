5dda83aeba52f96ee6cf361483e276b6a2bf270b
Denis Rouzaud <denis.rouzaud@gmail.com>
2014-02-25 21:15:28 +0100

    start win release batch file

--- win32/qtcreator.bat
+++ win32/qtcreator.bat
@@ -13,7 +13,7 @@ call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 @set INCLUDE=%INCLUDE%;%OSGEO4W_ROOT%\include;%OSGEO4W_ROOT%\apps\qgis-dev\include
 @set LIB=%LIB%;%OSGEO4W_ROOT%\lib;%OSGEO4W_ROOT%\apps\qgis-dev\lib
 
-path %SYSTEMROOT%\System32;%SYSTEMROOT%;%SYSTEMROOT%\System32\wbem;C:\CMake 2.8\bin;C:\GnuWin32\bin;C:\Progra~1\Git\bin;C:\Qt\qtcreator-3.0.1\bin;%PATH%
+path %OSGEO4W_ROOT%\bin;%SYSTEMROOT%\System32;%SYSTEMROOT%;%SYSTEMROOT%\System32\wbem;C:\Progra~1\Git\bin;C:\Qt\qtcreator-3.0.1\bin;%PATH%
 
 set VS90COMNTOOLS=C:\Program Files\Microsoft Visual Studio 9.0\Common7\Tools\
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
---
+++ win32/release.bat
@@ -0,0 +1,58 @@
+cd ../
+rmdir build-win /Q /S
+mkdir build-win
+
+copy ..\build-hfp-Desktop-Release\release\hfp.exe build-win\
+copy c:\OSGeo4W\bin\QtCore4.dll build-win\
+copy c:\OSGeo4W\bin\QtGui4.dll build-win\
+copy c:\OSGeo4W\bin\QtXml4.dll build-win\
+copy c:\OSGeo4W\bin\QtNetwork4.dll build-win\
+copy c:\OSGeo4W\bin\QtSvg4.dll build-win\
+copy c:\OSGeo4W\bin\QtWebKit4.dll build-win\
+
+copy c:\OSGeo4W\bin\zlib_osgeo.dll build-win\
+copy c:\OSGeo4W\bin\msvcr71.dll build-win\
+copy c:\OSGeo4W\bin\phonon4.dll build-win\
+copy c:\OSGeo4W\bin\proj.dll build-win\
+copy c:\OSGeo4W\bin\geos_c.dll build-win\
+copy c:\OSGeo4W\bin\gdal110.dll build-win\
+copy c:\OSGeo4W\bin\ogdi_32b1.dll build-win\
+copy c:\OSGeo4W\bin\libexpat.dll build-win\
+copy c:\OSGeo4W\bin\xerces-c_3_1.dll build-win\
+copy c:\OSGeo4W\bin\LIBPQ.dll build-win\
+copy c:\OSGeo4W\bin\SSLEAY32.dll build-win\
+copy c:\OSGeo4W\bin\LIBEAY32.dll build-win\
+copy c:\OSGeo4W\bin\krb5_32.dll build-win\
+copy c:\OSGeo4W\bin\comerr32.dll build-win\
+copy c:\OSGeo4W\bin\k5sprt32.dll build-win\
+copy c:\OSGeo4W\bin\gssapi32.dll build-win\
+copy c:\OSGeo4W\bin\hdf_fw.dll build-win\
+copy c:\OSGeo4W\bin\mfhdf_fw.dll build-win\
+copy c:\OSGeo4W\bin\jpeg_osgeo.dll build-win\
+copy c:\OSGeo4W\bin\jpeg12_osgeo.dll build-win\
+copy c:\OSGeo4W\bin\netcdf.dll build-win\
+copy c:\OSGeo4W\bin\geotiff.dll build-win\
+copy c:\OSGeo4W\bin\libtiff.dll build-win\
+copy c:\OSGeo4W\bin\sqlite3.dll build-win\
+copy c:\OSGeo4W\bin\spatialite4.dll build-win\
+copy c:\OSGeo4W\bin\freexl.dll build-win\
+copy c:\OSGeo4W\bin\iconv.dll build-win\
+copy c:\OSGeo4W\bin\libxml2.dll build-win\
+copy c:\OSGeo4W\bin\LIBMYSQL.dll build-win\
+copy c:\OSGeo4W\bin\hdf5.dll build-win\
+copy c:\OSGeo4W\bin\szip.dll build-win\
+copy c:\OSGeo4W\bin\libcurl.dll build-win\
+copy c:\OSGeo4W\bin\zlib1.dll build-win\
+
+
+
+copy C:\Progra~1\Git\bin\libiconv-2.dll build-win\
+copy C:\Progra~1\Git\bin\libintl-8.dll build-win\
+
+copy c:\OSGeo4W\apps\qgis-dev\bin\qgis_Core.dll build-win\
+copy c:\OSGeo4W\apps\qgis-dev\bin\qgis_gui.dll build-win\
+
+
+cd win32
+
+ 
